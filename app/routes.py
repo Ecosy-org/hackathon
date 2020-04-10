6b84dd223e5e1da74f9816201b62f6f8f095183c
sayamss <sawaisayam@gmail.com>
2020-04-11 01:51:41 +0530

    Template setup

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,13 @@
 from app import app
 
+from flask import (Flask,
+                   redirect,
+                   url_for,
+                   request,
+                   render_template)
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('home.html')
+
