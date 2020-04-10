673fdc8422a3c126373e9dc6c416c580bbc114c7
Uzay-G <uzgirit@gmail.com>
2020-04-10 23:25:05 +0200

    basic AQICN prototype

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
 from app import app
-
+import requests
+from flask import render_template
+import json
+import os
+TOKEN = os.environ['AQICN_TOKEN']
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html', token=TOKEN)
